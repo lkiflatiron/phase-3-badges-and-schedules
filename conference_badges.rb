3399ca3780c21a487b4c963babbf22775711425e
Louise Iyengar <louise.iyengar@flatironschool.com>
2022-02-20 14:05:15 -0500

    commit tree d41dc269b92b664a50b0a6070c379fa2482a2ef9

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,21 @@
 # Write your code here.
+def badge_maker person_name
+  "Hello, my name is #{person_name}."
+end
+
+def batch_badge_creator name_array
+  name_array.map { |name| badge_maker name }
+end
+
+def assign_rooms name_array
+  name_array.each.with_index(1).map do | name, index | 
+    "Hello, #{name}! You'll be assigned to room #{index}!"
+  end 
+end
+
+def printer name_array
+  batch_badge_creator(name_array).each { | badge| puts badge }
+  assign_rooms(name_array).each { | assignment | puts assignment }
+end
+
+printer(["Arel", "Carol"])
